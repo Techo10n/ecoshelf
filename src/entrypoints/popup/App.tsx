c92f6ee351ba7873fa3c96acbd86c6b73a4ed87f
Techo10n <trafficjammisterowl@gmail.com>
2025-05-23 14:28:57 -0700

    added black bg + green border

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -4,10 +4,9 @@ import wxtLogo from '/wxt.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center border border-green-500 w-full h-full bg-black">
       <h1 className="text-3xl font-bold text-white">
         Welcome to EcoShelf
       </h1>
